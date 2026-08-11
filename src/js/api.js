f09796488c1160bd846144794a524015d2bc78a0
Kasper Haugestøl <kasper@haugestol.com>
2026-08-11 13:51:38 +0200

    crud complete

--- src/js/api.js
+++ src/js/api.js
@@ -2,10 +2,42 @@ const BASE_URL = "http://localhost:3000/api/listings";
 
 export async function fetchListings() {
 	const res = await fetch(BASE_URL);
+
 	if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
 	return res.json();
 }
 
-// export async function createListing(input) { ... }   // POST /
-// export async function updateListing(id, input) { ... } // PATCH /:id
-// export async function deleteListing(id) { ... }       // DELETE /:id
+export async function createListing(input) {
+	const res = await fetch(BASE_URL, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(input),
+	});
+
+	if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
+	return res.json();
+}
+
+export async function updateListing(id, input) {
+	const res = await fetch(`${BASE_URL}/${id}`, {
+		method: "PATCH",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(input),
+	});
+
+	if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
+	return res.json();
+}
+
+export async function deleteListing(id) {
+	const res = await fetch(`${BASE_URL}/${id}`, {
+		method: "DELETE",
+	});
+
+	if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
+	return res.json();
+}
